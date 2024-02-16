d11743c60ad99a94950701d4fd2f75c3e82b688d
Newton-Masese <newtonmasese23@gmail.com>
2024-02-16 03:13:08 +0000

    added food interface and implemented it

--- src/main/java/com/kiosk/food/Food.java
+++ src/main/java/com/kiosk/food/Food.java
@@ -1,2 +1,22 @@
-package com.kiosk.food;public interface Food {
+package com.kiosk.food;
+
+/**
+ * Interface defining a generic menu Item,
+ * characterised by containing an amount of calories
+ * and by price
+ */
+public interface Food {
+    /**
+     * Getter method for the Price
+     *
+     * @return the price of this food item
+     */
+    public double getPrice();
+
+    /**
+     * Getter method for the calories
+     *
+     * @return the calories of this food item
+     */
+    public int getCalories();
 }
