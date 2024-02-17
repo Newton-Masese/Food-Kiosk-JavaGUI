e3ef73cba247c5d982b2d00371ea400b23aea697
Newton-Masese <newtonmasese23@gmail.com>
2024-02-17 19:28:24 +0000

    Add AdvancedMealController class, extending MealController with overridden method to add food items, considering diet attribute

--- src/main/java/com/kiosk/AdvancedMealController.java
+++ src/main/java/com/kiosk/AdvancedMealController.java
@@ -1,2 +1,45 @@
-package com.kiosk;public class AdvanceMealController {
+package com.kiosk;
+
+import com.kiosk.food.Food;
+
+/**
+ * A specialised controller that is aware of diet food
+ */
+public class AdvancedMealController extends MealController{
+    /**
+     * The constructor needs to get a model as its parameter as
+     * it will need to trigger changes on it.
+     *
+     * @param m the model
+     */
+    public AdvancedMealController(Meal m) {
+        super(m);
+    }
+
+    /**
+     * Method to add a food item to the model.
+     * This is overridden to default to the case when
+     * the food is not diet (attribute set to false)
+     *
+     * @param f the new food item
+     */
+    @Override
+    public void addFoodItem(Food f) {
+        this.addFoodItem(f, false);
+    }
+
+    /**
+     * Specialised method to add a food item to the model,
+     * which accounts for the fact that the food item might be a diet one.
+     *
+     * @param f the new food item
+     * @param isDiet true if the food is assumed to be diet
+     */
+    public void addFoodItem(Food f, boolean isDiet) {
+        if (isDiet) {
+            // TODO create a DietFood object and add it to the model
+        } else {
+            super.addFoodItem(f);
+        }
+    }
 }
