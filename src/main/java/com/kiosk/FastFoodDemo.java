2234d7066f6b2623d6f7b02666197c9e2a4238dc
Newton-Masese <newtonmasese23@gmail.com>
2024-02-17 19:29:25 +0000

    Demo class to run app

--- src/main/java/com/kiosk/FastFoodDemo.java
+++ src/main/java/com/kiosk/FastFoodDemo.java
@@ -1,2 +1,9 @@
-package com.kiosk;public class FastFoodDemo {
+package com.kiosk;
+
+public class FastFoodDemo {
+
+    public static void main(String[] args){
+        Meal model = new Meal();
+        MealController controller = new MealController(model);
+    }
 }
