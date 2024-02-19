f67b350f427ad0329409821b808f55d8a4c01307
Newton-Masese <newtonmasese23@gmail.com>
2024-02-19 16:48:55 +0000

    exteded class with JLabel and implemented PropertyChangeListener for Displaying Meal items with dynamic updates

--- src/main/java/com/kiosk/gui/MealItemsView.java
+++ src/main/java/com/kiosk/gui/MealItemsView.java
@@ -1,4 +1,48 @@
 package com.kiosk.gui;
 
-public class MealItemsView {
+import com.kiosk.MealChangeEvent;
+import com.kiosk.MealModel;
+import com.kiosk.food.Food;
+
+import javax.swing.*;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+public class MealItemsView extends JPanel implements PropertyChangeListener {
+
+    private final JTextArea textArea;
+
+    public MealItemsView(){
+        this.setBorder(BorderFactory.createTitledBorder("Meal Items"));
+        textArea = new JTextArea(10, 30);
+        textArea.setLineWrap(true);
+        textArea.setWrapStyleWord(true);
+        JScrollPane scrollPane = new JScrollPane(textArea);
+
+        this.add(scrollPane);
+    }
+
+    /**
+     * This method gets called when a bound property is changed.
+     *
+     * @param evt A PropertyChangeEvent object describing the event source
+     *            and the property that has changed.
+     */
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+        if(evt instanceof MealChangeEvent){
+            String propertyName = evt.getPropertyName();
+
+            if (MealModel.ITEM_LIST_PROPERTY.equals(propertyName)){
+                Food newItem = (Food) evt.getNewValue();
+                String descr = newItem + ": £" + newItem.getPrice() + " ["+ newItem.getCalories() + " cal]";
+                this.textArea.append(descr + "\n");
+                repaint();
+            } else if (MealModel.CHECKOUT_PROPERTY.equals(propertyName)) {
+                this.textArea.setText("");
+            }
+        }else {
+            throw new IllegalArgumentException("Unknown event for property: " + evt.getPropertyName());
+        }
+    }
 }
