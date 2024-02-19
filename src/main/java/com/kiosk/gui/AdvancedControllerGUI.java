18eea8567e5e19b623bfb431843092e319199d57
Newton-Masese <newtonmasese23@gmail.com>
2024-02-19 16:55:00 +0000

    Added a basic GUI with buttoms to add food items and place orders

--- src/main/java/com/kiosk/gui/AdvancedControllerGUI.java
+++ src/main/java/com/kiosk/gui/AdvancedControllerGUI.java
@@ -1,4 +1,91 @@
 package com.kiosk.gui;
 
-public class AdvancedControllerGUI {
+import com.kiosk.AdvancedMealController;
+import com.kiosk.food.Beverage;
+import com.kiosk.food.Burger;
+import com.kiosk.food.Dessert;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class AdvancedControllerGUI extends BasicControllerGui {
+
+    private final JCheckBox beverageCheckBox = new JCheckBox("diet");
+    private final JCheckBox burgerCheckBox = new JCheckBox("diet");
+    private final JCheckBox dessertCheckBox = new JCheckBox("diet");
+
+    /**
+     * Creates a GUI for a controller element of the specialised kind.
+     *
+     * @param controller the advanced controller
+     */
+    public AdvancedControllerGUI(AdvancedMealController controller) {
+        super(controller);
+
+        JPanel checkBoxPanel = new JPanel();
+        checkBoxPanel.setLayout(new BoxLayout(checkBoxPanel, BoxLayout.Y_AXIS));
+
+        Dimension checkBoxSize = new Dimension(100, 30);
+        beverageCheckBox.setMaximumSize(checkBoxSize);
+        burgerCheckBox.setMaximumSize(checkBoxSize);
+        dessertCheckBox.setMaximumSize(checkBoxSize);
+
+        checkBoxPanel.add(beverageCheckBox);
+        checkBoxPanel.add(burgerCheckBox);
+        checkBoxPanel.add(dessertCheckBox);
+
+        mainPanel.add(checkBoxPanel);
+    }
+
+    /**
+     * The method to be called when the "Add beverage" button is pressed.
+     * The method is overridden to check whether the beverage is diet
+     *
+     * @param b the new beverage
+     */
+    protected void addFoodItem(Beverage b) {
+        AdvancedMealController advController = (AdvancedMealController) controller;
+        advController.addFoodItem(b, beverageCheckBox.isSelected());
+        beverageCheckBox.setSelected(false);
+    }
+
+    /**
+     * The method to be called when the "Add burger" button is pressed
+     * The method is overridden to check whether the burger is diet
+     *
+     * @param b the new burger
+     */
+    protected void addFoodItem(Burger b) {
+        AdvancedMealController advController = (AdvancedMealController) controller;
+        advController.addFoodItem(b, burgerCheckBox.isSelected());
+        burgerCheckBox.setSelected(false);
+    }
+
+    /**
+     * The method to be called when the "Add dessert" button is pressed
+     * The method is overridden to check whether the dessert is diet
+     *
+     * @param d the new dessert
+     */
+    protected void addFoodItem(Dessert d) {
+        AdvancedMealController advController = (AdvancedMealController) controller;
+        advController.addFoodItem(d, dessertCheckBox.isSelected());
+        dessertCheckBox.setSelected(false);
+    }
+
+    /**
+     * The method to be called when the "Place order" button is pressed.
+     * The method is overridden to reset the checkboxes
+     *
+     */
+    @Override
+    protected void reset() {
+        beverageCheckBox.setSelected(false);
+        burgerCheckBox.setSelected(false);
+        dessertCheckBox.setSelected(false);
+
+        super.reset();
+    }
+
+
 }
--- src/main/java/com/kiosk/gui/BasicControllerGui.java
+++ src/main/java/com/kiosk/gui/BasicControllerGui.java
@@ -1,4 +1,102 @@
 package com.kiosk.gui;
 
-public class BasicControllerGui {
+import com.kiosk.MealController;
+import com.kiosk.food.Beverage;
+import com.kiosk.food.Burger;
+import com.kiosk.food.Dessert;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class BasicControllerGui extends JPanel {
+
+    protected final MealController controller;
+    protected JPanel mainPanel;
+    /**
+     * Creates a GUI for a controller element
+     *
+     * @param controller the controller
+     */
+    public BasicControllerGui(MealController controller){
+        this.controller = controller;
+
+        this.setLayout(new BorderLayout());
+
+        mainPanel = new JPanel();
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.X_AXIS));
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.Y_AXIS));
+
+        JButton addBeverageButton = new JButton("Add Beverage");
+        addBeverageButton.addActionListener( e -> {
+            Beverage b = new Beverage("Coke");
+            this.addFoodItem(b);
+        });
+        JButton addBurgerButton = new JButton("Add Burger");
+        addBurgerButton.addActionListener(e -> {
+            Burger b = new Burger("Hamburger");
+            this.addFoodItem(b);
+        });
+
+        JButton addDessertButton = new JButton("Add Dessert");
+        addDessertButton.addActionListener(e -> {
+            Dessert d = new Dessert("Sundae");
+            this.addFoodItem(d);
+        });
+
+        Dimension buttonSize = new Dimension(150, 30);
+        addBeverageButton.setMaximumSize(buttonSize);
+        addBurgerButton.setMaximumSize(buttonSize);
+        addDessertButton.setMaximumSize(buttonSize);
+
+        buttonPanel.add(addBeverageButton);
+        buttonPanel.add(addBurgerButton);
+        buttonPanel.add(addDessertButton);
+
+        mainPanel.add(buttonPanel);
+        this.add(mainPanel, BorderLayout.CENTER);
+
+        JButton placeOrderButton = new JButton("Place order");
+        placeOrderButton.addActionListener(e -> {
+            this.reset();
+        });
+
+        this.add(placeOrderButton, BorderLayout.SOUTH);
+    }
+    /**
+     * The method to be called when the "Add beverage" button is pressed
+     *
+     * @param b the new beverage
+     */
+    protected void addFoodItem(Beverage b) {
+        controller.addFoodItem(b);
+    }
+
+    /**
+     * The method to be called when the "Add burger" button is pressed
+     *
+     * @param b the new burger
+     */
+    protected void addFoodItem(Burger b) {
+        controller.addFoodItem(b);
+    }
+
+    /**
+     * The method to be called when the "Add dessert" button is pressed
+     *
+     * @param d the new dessert
+     */
+    protected void addFoodItem(Dessert d) {
+        controller.addFoodItem(d);
+    }
+
+    /**
+     * The method to be called when the "Place order" button is pressed
+     */
+    protected void reset() {
+        controller.reset();
+    }
+
+
 }
